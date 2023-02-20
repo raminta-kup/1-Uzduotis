6568bad97d725050103dde41483f1e71cc1edd6f
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-02-20 18:07:02 +0200

    Formatting and changed styles

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ const converterForm = document.querySelector("form");
 const calculateBtn = document.querySelector("#submit-btn");
 const searchInput = document.querySelector("#search");
 const output = document.querySelector("#output");
+const resultTable = document.createElement("table");
+const trTitles = document.createElement("tr");
+const thKg = document.createElement("th");
+const thLb = document.createElement("th");
+const thGrams = document.createElement("th");
+const thOz = document.createElement("th");
+const errorSpan = document.createElement("span");
 
-output.style.display = "flex";
-output.style.flexDirection = "column";
-output.style.gap = "20px";
-output.style.maxWidth = "80%";
-output.style.margin = "50px auto";
+output.prepend(errorSpan);
+output.append(resultTable);
+resultTable.append(trTitles);
+trTitles.append(thKg, thLb, thGrams, thOz);
 
 function toLb(num) {
     return num * 2.2046;
@@ -36,48 +42,53 @@ function resultsToOutput() {
 
     calculateBtn.addEventListener("click", (e) => {
         e.preventDefault();
-
         const lbsResult = toLb(searchInput.value);
         const gramsResult = toGrams(searchInput.value);
         const ozResult = toOz(searchInput.value);
 
         if (Number(searchInput.value)) {
-
-            createTable(lbsResult, gramsResult, ozResult);
-
+            insertTableData(lbsResult, gramsResult, ozResult);
+            errorSpan.textContent = "";
         } else {
-            const errorSpan = document.createElement("span");
-            output.append(errorSpan);
             errorSpan.textContent = "* Not a number. Please Try again";
-            errorSpan.style.color = "red";
-            errorSpan.style.textAlign = "center";
         }
     })
 }
 
-function createTable(lb, grams, oz) {
-    const resultTable = document.createElement("table");
-    const trTitles = document.createElement("tr");
-    const thLb = document.createElement("th");
-    const thGrams = document.createElement("th");
-    const thOz = document.createElement("th");
+function insertTableData(lb, grams, oz) {
     const trResults = document.createElement("tr");
+    const tdKg = document.createElement("td");
     const tdLb = document.createElement("td");
     const tdGrams = document.createElement("td");
     const tdOz = document.createElement("td");
 
-    resultTable.style.textAlign = "center"
-    thLb.textContent = "Lbs";
-    thGrams.textContent = "Grams";
-    thOz.textContent = "Oz";
+    tdKg.textContent = searchInput.value;
     tdLb.textContent = lb;
     tdGrams.textContent = grams;
     tdOz.textContent = oz;
+    tdGrams.style.padding = "16px 0";
+    tdKg.style.padding = "16px 0";
+    tdOz.style.padding = "16px 0";
+    tdLb.style.padding = "16px 0";
 
-    output.append(resultTable);
-    resultTable.append(trTitles, trResults);
-    trTitles.append(thLb, thGrams, thOz);
-    trResults.append(tdLb, tdGrams, tdOz);
+    resultTable.append(trResults);
+    trResults.append(tdKg, tdLb, tdGrams, tdOz);
 }
 
 resultsToOutput();
+
+output.style.maxWidth = "80%";
+output.style.margin = "50px auto";
+resultTable.style.padding = "24px 0";
+resultTable.style.backgroundColor = "white";
+resultTable.style.textAlign = "center";
+resultTable.style.width = "100%";
+thLb.textContent = "Lbs";
+thGrams.textContent = "Grams";
+thOz.textContent = "Oz";
+thKg.textContent = "Kg";
+errorSpan.style.color = "red";
+errorSpan.style.textAlign = "center";
+errorSpan.style.padding = "16px 0";
+errorSpan.style.display = "block";
+errorSpan.style.fontSize = "20px";
